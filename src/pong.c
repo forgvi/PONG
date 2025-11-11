fd97da2227d6ebdec0266f39b7f6b5ee52bbcc74
forg_vi <igor@forg-pc.localdomain>
2025-11-11 20:28:42 +0600

    check .gitignore

--- src/pong.c
+++ src/pong.c
@@ -3,35 +3,35 @@
 void print_field(int width, int height);
 
 int main (void) 
-{
-  char fild_array[width][height]; 
-  int field_x = 80;
-  int field_y = 25;
-  char exit;
-  do {
-    scanf("%c", &exit);
-    if (exit == 's') {
-      print_field(field_x, field_y);
-      printf("\033[2J\033[H");
-    }
-  } while (exit != 'e');
+{ // reffactor
+  // char fild_array[width][height]; 
+  // int field_x = 80;
+  // int field_y = 25;
+  // char exit;
+  // do {
+  //   scanf("%c", &exit);
+  //   if (exit == 's') {
+  //     print_field(field_x, field_y);
+  //     printf("\033[2J\033[H");
+  //   }
+  // } while (exit != 'e');
   
 
   return  0;
 }
-
-void init_field(int width, int height) 
-{
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      if (i == 0 || i == (height - 1)) array[i][j] = '-';
-      else if (j == 0 || j == (width -1)) printf("%c", '|');
-      else if ((i > 0 && i < width) && (j == (width / 2) - 1)) printf("%c", '|');
-      else printf("%c", ' ');
-    }
-    printf("\n");
-  }
-}
+// refactor
+// void init_field(int width, int height) 
+// {
+//   for (int i = 0; i < height; i++) {
+//     for (int j = 0; j < width; j++) {
+//       if (i == 0 || i == (height - 1)) array[i][j] = '-';
+//       else if (j == 0 || j == (width -1)) printf("%c", '|');
+//       else if ((i > 0 && i < width) && (j == (width / 2) - 1)) printf("%c", '|');
+//       else printf("%c", ' ');
+//     }
+//     printf("\n");
+//   }
+// }
 
 void print_field(int width, int height) 
 {
